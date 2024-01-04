f929c7c7730c076e5cb3e0f335de7ca3e8471266
abd-Kabir <kabirumarov@gmail.com>
2024-01-04 23:54:43 +0500

    paycom models changes

--- apps/payment/models.py
+++ apps/payment/models.py
@@ -15,7 +15,7 @@ class Transaction(BaseModel):
         (CANCELED, 'canceled')
     )
     status = models.CharField(choices=STATUS, default='processing', max_length=55)
-    payment_id = models.IntegerField(null=True, blank=True)
+    payment_id = models.CharField(max_length=255, null=True, blank=True)
     order_key = models.CharField(max_length=50, unique=True, editable=False)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     create_datetime = models.DateTimeField(null=True, blank=True)
