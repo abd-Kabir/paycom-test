e927056baa410f9916a77bf371a23e0a036c906b
abd-Kabir <kabirumarov@gmail.com>
2024-01-04 23:31:04 +0500

    temp settings

--- config/settings.py
+++ config/settings.py
@@ -13,10 +13,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv('SECRET_KEY')
+SECRET_KEY = "django-insecure-cv%@zb-x#*+^6^6a37x)@7wi5r2o0hbkr!_7^sf**vyw^(h7()"
+# SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(getenv('DEBUG')))
+DEBUG = True
+# DEBUG = bool(int(getenv('DEBUG')))
 
 ALLOWED_HOSTS = ["*"]
 
