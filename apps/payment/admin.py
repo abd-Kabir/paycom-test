f52db4188e0aa2aaab5b04ee468d5f365990ec51
abd-Kabir <kabirumarov@gmail.com>
2024-01-08 17:22:30 +0500

    bug fix

--- apps/payment/admin.py
+++ apps/payment/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
-
 from apps.payment.models import Transaction, Package
 
-admin.site.register(User)
 admin.site.register(Package)
 admin.site.register(Transaction)
