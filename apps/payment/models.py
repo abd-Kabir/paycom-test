456d060fdc3601fe7d73c18e9a38616c5efaa11d
abd-Kabir <kabirumarov@gmail.com>
2024-01-08 17:56:24 +0500

    bug fix

--- apps/payment/models.py
+++ apps/payment/models.py
@@ -9,6 +9,9 @@ class Package(BaseModel):
     name = models.CharField(max_length=55)
     amount = models.PositiveIntegerField()
 
+    def __str__(self):
+        return f"{self.name}"
+
     class Meta:
         db_table = 'Package'
 
