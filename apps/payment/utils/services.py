c60c30176b641cfb1b5a7a1a0d3b0d50de909c82
abd-Kabir <kabirumarov@gmail.com>
2024-01-08 18:08:11 +0500

    perform transaction

--- apps/payment/utils/services.py
+++ apps/payment/utils/services.py
@@ -19,7 +19,6 @@ def check_perform_transaction(params) -> dict:
 
 
 def create_transaction(params) -> dict:
-    create_datetime = datetime.now()
     amount = params.get('amount')
     transaction_key = params.get('id')
     if transaction_key in Transaction.objects.values_list('transaction_key', flat=True):
@@ -40,7 +39,7 @@ def create_transaction(params) -> dict:
                     }
                 }
             }
-        instance = Transaction.objects.create(create_datetime=create_datetime,
+        instance = Transaction.objects.create(create_datetime=datetime.now(),
                                               transaction_key=transaction_key,
                                               amount=amount,
                                               state=1,
@@ -57,13 +56,28 @@ def create_transaction(params) -> dict:
 
 
 def perform_transaction(params) -> dict:
-    transaction_id = randint(100_000, 999_999)
-    perform_time = int(datetime.now().timestamp() * 1000)
+    transaction_key = params.get('id')
+    instance = Transaction.objects.filter(transaction_key=transaction_key)
+    if not instance:
+        return {
+            "error": {
+                "code": -31003,
+                "message": {
+                    "uz": 'Buyurtma topilmadi',
+                    "ru": 'Транзакция не найдена',
+                    "en": 'Transaction not found'
+                }
+            }
+        }
+    instance = instance.first()
+    instance.perform_datetime = datetime.now()
+    instance.state = 2
+    instance.save()
     return {
         "result": {
-            "perform_time": perform_time,
-            "transaction": transaction_id,
-            "state": 2
+            "perform_time": instance.perform_datetime.timestamp() * 1000,
+            "transaction": transaction_key,
+            "state": instance.state
         }
     }
 
